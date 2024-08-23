c4dbe415ce15b6d7817963024fcdc3d194f70b58
lyc_PC <yongchunl@student.unimelb.edu.au>
2024-08-23 14:16:56 +1000

    Update ErrorResponse.java

--- demo/src/main/java/com/AI/Budgerigar/chatbot/DTO/ErrorResponse.java
+++ demo/src/main/java/com/AI/Budgerigar/chatbot/DTO/ErrorResponse.java
@@ -1,17 +1,20 @@
 package com.AI.Budgerigar.chatbot.DTO;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+
+@Getter
 public class ErrorResponse {
+    private static final Logger logger = LoggerFactory.getLogger(ErrorResponse.class);
 
+
+    // Getter and Setter
     private String error;
 
     public ErrorResponse(String error) {
         this.error = error;
-        System.out.println("ErrorResponse: " + error);
-    }
-
-    // Getter and Setter
-    public String getError() {
-        return error;
+        logger.error(error);
     }
 
     public void setError(String error) {
