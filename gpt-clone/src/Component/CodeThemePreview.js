0173c9df8eb7be2f9b6b6c9859f35776c1685cca
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-12-26 00:04:02 +0800

    222

--- gpt-clone/src/Component/CodeThemePreview.js
+++ gpt-clone/src/Component/CodeThemePreview.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { CODE_EXAMPLES } from '../constants/codeExamples';
 import './CodeThemePreview.css';
 
+// 使用localStorage来保存上次选择的语言
+const LAST_SELECTED_LANGUAGE_KEY = 'lastSelectedCodeLanguage';
+
 const CodeThemePreview = ({ theme, isZH }) => {
-    const [selectedLanguage, setSelectedLanguage] = useState('javascript');
+    const [selectedLanguage, setSelectedLanguage] = useState(() => {
+        // 从localStorage读取上次选择的语言，如果没有则默认为javascript
+        return localStorage.getItem(LAST_SELECTED_LANGUAGE_KEY) || 'javascript';
+    });
+    
     const languages = Object.keys(CODE_EXAMPLES);
+
+    // 当语言改变时保存到localStorage
+    useEffect(() => {
+        localStorage.setItem(LAST_SELECTED_LANGUAGE_KEY, selectedLanguage);
+    }, [selectedLanguage]);
 
     return (
         <div className="code-theme-preview">
