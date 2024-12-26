55cb9961568bd27538e95d9e328c07ca262cd9a2
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-12-26 12:24:11 +0800

    Update UserServiceImpl.java

--- demo/src/main/java/com/AI/Budgerigar/chatbot/Services/impl/UserServiceImpl.java
+++ demo/src/main/java/com/AI/Budgerigar/chatbot/Services/impl/UserServiceImpl.java
@@ -107,7 +107,9 @@ public class UserServiceImpl implements userService {
                     adminWaitingListRedisDAO.addAdminToWaitingList(inviteToken, uuid);
                     log.info("Invite tk for" + username + ": " + inviteToken);
 
-                    String confirmationLink = request.getScheme() + "://" + request.getServerName()
+                    String serverPort = env.getProperty("server.port", "3000");
+                    String confirmationLink = request.getScheme() + "://" + request.getServerName() 
+                            + (request.getServerName().equals("localhost") ? ":" + serverPort : "")
                             + "/user/register/confirm/" + inviteToken;
                     String emailContent = "<p>Please confirm your admin registration by clicking the following link:</p>"
                             + "<p><a href='" + confirmationLink + "'>" + confirmationLink + "</a></p>";
